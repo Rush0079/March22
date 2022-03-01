59caf54d698ac674ca9b94e8d634361a7dd00a50
Kiran Waghmare <72081819+Kiranwaghmare123@users.noreply.github.com>
2022-03-01 14:36:32 +0530

    Add files via upload

---
+++ Logic Building/Day1/First1.java
@@ -0,0 +1,12 @@
+class First1
+{
+	public static void main(String args [])
+	{
+		System.out.println("Hello Java....");
+		System.out.print("Hello Java....");
+		System.out.print("Hello Java....");
+		System.out.println("Hello Java....");
+		System.out.println("Hello Java....");
+		
+	}
+}
